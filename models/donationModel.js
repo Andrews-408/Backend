1fe0f5e0944e9b29f14a63875afa8c6960d12afe
enoch Sarkodie <enochsarkodie07@gmail.com>
2023-05-29 10:37:27 +0000

    implemented donation model

---
+++ models/donationModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+
+const donationSchema = new mongoose.Schema({
+    donationType : {
+        type: String,
+        required:[true,'Specify the type of donation']
+    },
+    donationId : {
+        type :String,
+        required: [true,'Donation ID is required'],
+        unique : true
+    },
+    donatedBy :{
+        type : String,
+        required: [true,'Donor name required']
+    },
+    date: {
+        type: Date,
+        required:[false]
+    },
+    description: {
+        type: String,
+        required:[false]
+    },
+    itemPhoto: {
+        type: String,
+        required:[true]
+    },
+    location :{
+        type : String,
+        required: [true,'Please provide location for pick-up']
+    },
+}, {timestamps : true})
+
+
+const Donations = mongoose.model("Donations",donationSchema);
+module.exports = Donations;
