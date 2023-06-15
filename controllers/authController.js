8b5e1504db609f00576d7667e51c827706b17f97
andrews-408 <yeboahandrews408@gmail.com>
2023-06-15 14:01:11 +0000

    permission and authorization using user roles implementation

--- controllers/authController.js
+++ controllers/authController.js
@@ -120,6 +120,16 @@ exports.protect = (model) => catchAsync(async(req, res, next)=>{
         next()
 })
 
+// restrict access to certain routes
+exports.restrictTo = (...roles) => {
+    return(req, res, next) => {
+        if(!roles.includes(req.user.role)){
+            return next(new AppError("You do not have permission to perform this action", 403))
+        }
+
+        next()
+    }
+}
 
 // resetToken mailing controller
 
